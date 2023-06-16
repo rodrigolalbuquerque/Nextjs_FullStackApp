3dbffbf57ab23af9f5d4a9276ec2f635afd04f9a
Rodrigo Lomar de Albuquerque <rodrigolomardev@gmail.com>
2023-06-16 16:20:27 -0300

    adds login with google

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react'
 import styles from './page.module.css'
 import useSWR from 'swr'
+import { useSession } from 'next-auth/react'
 
 async function getData(id) {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
@@ -41,10 +42,11 @@ function Dashboard() {
   //   getData()
   // }, [])
 
+  const session = useSession();
+  console.log(session);
+
   const fetcher = (...args) => fetch(...args).then(res => res.json());
   const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts/', fetcher)
-
-  console.log(data);
 
   return (
     <div className={styles.container}>Dashboard</div>
